c7bceb2ad44b4928ba627de2d0006ff3a53effb8
Rby1702 <deveraruby1234517@gmail.com>
2017-12-06 09:24:59 +0800

    Update

---
+++ js/process.js
@@ -0,0 +1,272 @@
+let ruby = {
+    d:function(){
+let p=<div>
+    <center><br/><br/><br/>
+    <h1><b><font color="blue"> LEARN FUN </font></b></h1><br/>
+        <button><a href="#" onClick={ruby.r}><h2> SHAPES </h2></a></button><br/><br/>
+        <button><a href="#" onClick={ruby.u}><h2> FRUITS </h2></a></button><br/><br/>
+        <button><a href="#" onClick={ruby.b}><h2> NUMBERS </h2></a></button><br/><br/></center></div>;
+ReactDOM.render(p, document.getElementById("r"));
+
+    },
+    r:function(){
+        let p = <div style={{background:"skyblue"}}> 
+<marquee behavior="scroll" direction="left">
+<img src="img/shapes1.jpg" width="300"  height="200" alt="Swimming fish"/>
+</marquee>
+<center><br/><br/><br/>
+<h1><b> <font color="blue"> LEARN FUN </font></b></h1>
+<div class="desktop">
+  <a target="_blank" href="circle.jpg">
+    <img src="circle.jpg" alt="Fjords" width="300" height="200"/>
+  </a>
+  <div class="desc">CIRCLE</div>
+</div><br/><br/><br/><br/><br/>
+
+<div class="desktop">
+  <a target="_blank" href="2.png">
+    <img src="2.png" alt="Forest" width="300" height="200"/>
+  </a>
+  <div class="desc">RECTANGLE</div>
+</div><br/><br/><br/><br/><br/>
+
+<div class="desktop">
+  <a target="_blank" href="3.png">
+    <img src="3.png" alt="Northern Lights" width="300" height="200"/>
+  </a>
+  <div class="desc">HEART</div>
+</div><br/><br/><br/><br/><br/>
+
+<div class="desktop">
+  <a target="_blank" href="4.png">
+    <img src="4.png" alt="Mountains" width="300" height="200"/>
+  </a>
+  <div class="desc">TRIANGLE</div>
+</div><br/><br/><br/><br/><br/>
+
+<div class="desktop">
+  <a target="_blank" href="5.png">
+    <img src="5.png" alt="Mountains" width="300" height="200"/>
+  </a>
+  <div class="desc">STAR</div>
+</div><br/><br/><br/><br/><br/>
+
+<div class="desktop">
+  <a target="_blank" href="6.png">
+    <img src="6.png" alt="Mountains" width="300" height="200"/>
+  </a>
+  <div class="desc">SQUARE</div>
+</div><br/><br/><br/><br/><br/>
+
+<div class="desktop">
+  <a target="_blank" href="7.png">
+    <img src="7.png" alt="Mountains" width="300" height="200"/>
+  </a>
+  <div class="desc">DIAMOND</div>
+</div><br/><br/><br/><br/><br/>
+
+<div class="desktop">
+  <a target="_blank" href="oval.jpg">
+    <img src="oval.jpg" alt="Mountains" width="300" height="200"/>
+  </a>
+  <div class="desc">OVAL</div>
+</div><br/><br/><br/><br/><br/>
+
+<div class="desktop">
+  <a target="_blank" href="hexagon.jpg">
+    <img src="hexagon.jpg" alt="Mountains" width="300" height="200"/>
+  </a>
+  <div class="desc">HEXAGON</div>
+</div><br/><br/><br/><br/><br/>
+
+<div class="desktop">
+  <a target="_blank" href="octagon.png">
+    <img src="octagon.png" alt="Mountains" width="300" height="200"/>
+  </a>
+  <div class="desc">OCTAGON</div>
+  </div><br/><br/>
+    <a href="#" onClick={ruby.d}><div class="ks-demo-icon col-auto tablet-auto"><div class="icon" style={{color:"orange"}}><i class="material-icons">home</i></div></div></a>Back to home</center>
+    </div>;
+            ReactDOM.render(p,document.getElementById('r'));
+    },
+    u:function(){
+        let p = <div style={{background:"skyblue"}}>
+<marquee behavior="scroll" direction="left">
+<img src="img/fruits1.jpg" width="300"  height="200" alt="Swimming fish"/>
+</marquee>
+<center>
+<h1><b> <font color="blue"> LEARN FUN </font></b></h1>
+<div class="desktop">
+  <a target="_blank" href="f1.jpg">
+    <img src="img/f1.jpg" alt="Fjords" width="300" height="200"/>
+  </a>
+  <div class="desc">APPLE</div>
+</div><br/><br/><br/><br/><br/>
+
+<div class="desktop">
+  <a target="_blank" href="f2.jpg">
+    <img src="img/f2.jpg" alt="Forest" width="300" height="200"/>
+  </a>
+  <div class="desc">ORANGE</div>
+</div><br/><br/><br/><br/><br/>
+
+<div class="desktop">
+  <a target="_blank" href="f3.jpg">
+    <img src="img/f3.jpg" alt="Forest" width="300" height="200"/>
+  </a>
+  <div class="desc">GRAPES</div>
+</div><br/><br/><br/><br/><br/>
+
+<div class="desktop">
+  <a target="_blank" href="f4.jpg">
+    <img src="img/f4.jpg" alt="Northern Lights" width="300" height="200"/>
+  </a>
+  <div class="desc">STRAWBERRY</div>
+</div><br/><br/><br/><br/><br/>
+
+<div class="desktop">
+  <a target="_blank" href="f5.jpg">
+    <img src="img/f5.jpg" alt="Mountains" width="300" height="200"/>
+  </a>
+  <div class="desc">BANANA</div>
+</div><br/><br/><br/><br/><br/>
+
+<div class="desktop">
+  <a target="_blank" href="f6.jpg">
+    <img src="img/f6.jpg" alt="Mountains" width="300" height="200"/>
+  </a>
+  <div class="desc">AVOCADO</div>
+</div><br/><br/><br/><br/><br/>
+
+<div class="desktop">
+  <a target="_blank" href="f7.jpg">
+    <img src="img/f7.jpg" alt="Mountains" width="300" height="200"/>
+  </a>
+  <div class="desc">CHERRY</div>
+</div><br/><br/><br/><br/><br/>
+
+<div class="desktop">
+  <a target="_blank" href="f8.jpg">
+    <img src="img/f8.jpg" alt="Mountains" width="300" height="200"/>
+  </a>
+  <div class="desc">GUAVA</div>
+</div><br/><br/><br/><br/><br/>
+
+<div class="desktop">
+  <a target="_blank" href="f9.jpg">
+    <img src="img/f9.jpg" alt="Mountains" width="300" height="200"/>
+  </a>
+  <div class="desc">MANGO</div>
+</div><br/><br/><br/><br/><br/>
+
+<div class="desktop">
+  <a target="_blank" href="f10.jpg">
+    <img src="img/f10.jpg" alt="Mountains" width="300" height="200"/>
+  </a>
+  <div class="desc">RAMBUTAN</div>
+</div><br/><br/><br/><br/><br/>
+<a href="#" onClick={ruby.d}><div class="ks-demo-icon col-auto tablet-auto"><div class="icon" style={{color:"orange"}}><i class="material-icons">home</i></div></div></a>Back to home</center>  
+    </div>;
+            ReactDOM.render(p,document.getElementById('r'));
+    },
+    b:function(){
+        let p = <div style={{background:"skyblue"}}>
+<marquee behavior="scroll" direction="left">
+<img src="img/numbers1.jpg" width="300"  height="200"  alt="Swimming fish"/>
+</marquee>
+<center>
+<h1><b> <font color="blue"> LEARN FUN </font></b></h1>
+</center>
+<center>
+<div class="desktop">
+  <a target="_blank" href="n0.jpg">
+    <img src="img/n0.jpg" alt="Fjords" width="300" height="200"/>
+  </a>
+  <div class="desc">ZERO</div>
+</div><br/><br/><br/><br/><br/>
+
+<div class="desktop">
+  <a target="_blank" href="n1.jpg">
+    <img src="img/n1.jpg" alt="Forest" width="300" height="200"/>
+  </a>
+  <div class="desc">ONE</div>
+</div><br/><br/><br/><br/><br/>
+
+<div class="desktop">
+  <a target="_blank" href="n2.jpg">
+    <img src="img/n2.jpg" alt="Forest" width="300" height="200"/>
+  </a>
+  <div class="desc">TWO</div>
+</div><br/><br/><br/><br/><br/>
+
+<div class="desktop">
+  <a target="_blank" href="n3.jpg">
+    <img src="img/n3.jpg" alt="Northern Lights" width="300" height="200"/>
+  </a>
+  <div class="desc">THREE</div>
+</div><br/><br/><br/><br/><br/>
+
+<div class="desktop">
+  <a target="_blank" href="n4.jpg">
+    <img src="img/n4.jpg" alt="Mountains" width="300" height="200"/>
+  </a>
+  <div class="desc">FOUR</div>
+</div><br/><br/><br/><br/><br/>
+
+<div class="desktop">
+  <a target="_blank" href="n5.jpg">
+    <img src="img/n5.jpg" alt="Mountains" width="300" height="200"/>
+  </a>
+  <div class="desc">FIVE</div>
+</div><br/><br/><br/><br/><br/>
+
+<div class="desktop">
+  <a target="_blank" href="n6.jpg">
+    <img src="img/n6.jpg" alt="Mountains" width="300" height="200"/>
+  </a>
+  <div class="desc">SIX</div>
+</div><br/><br/><br/><br/><br/>
+
+<div class="desktop">
+  <a target="_blank" href="n7.jpg">
+    <img src="img/n7.jpg" alt="Mountains" width="300" height="200"/>
+  </a>
+  <div class="desc">SEVEN</div>
+</div><br/><br/><br/><br/><br/>
+
+<div class="desktop">
+  <a target="_blank" href="n8.jpg">
+    <img src="img/n8.jpg" alt="Mountains" width="300" height="200"/>
+  </a>
+  <div class="desc">EIGHT</div>
+</div><br/><br/><br/><br/><br/>
+
+<div class="desktop">
+  <a target="_blank" href="n9.jpg">
+    <img src="img/n9.jpg" alt="Mountains" width="300" height="200"/>
+  </a>
+  <div class="desc">NINE</div>
+</div><br/><br/><br/><br/><br/>
+    <a href="#" onClick={ruby.d}><div class="ks-demo-icon col-auto tablet-auto"><div class="icon" style={{color:"orange"}}><i class="material-icons">home</i></div></div></a>Back to home</center>  
+    </div>;
+            ReactDOM.render(p,document.getElementById('r'));
+    },
+    v:function(){
+let p=<center>
+        <div><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+            <h1 style={{color:"cyan"}}>Welcome to my application</h1>
+        </div></center>;
+ReactDOM.render(p,document.getElementById("r"));
+
+    }
+}
+
+ruby.v();
+setTimeout(function(){
+    ruby.d();
+},3500);
+            
+
+            
+
+            
